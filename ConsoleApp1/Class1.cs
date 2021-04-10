fb7ae2d35ec7b39c8ae624ad20948242c6034277
Prakash <virgoprak@gmail.com>
2021-04-10 13:12:22 +0530

    in

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b;
+
+            b = a++;
+            Console.WriteLine("a is : {0}", a);
+            Console.WriteLine("b is : {0}", b);
+
+            b = ++a;
+            Console.WriteLine("a is : {0}", a);
+            Console.WriteLine("b is : {0}", b);
+
+            b = a--;
+            Console.WriteLine("a is : {0}", a);
+            Console.WriteLine("b is : {0}", b);
+
+            b = --a;
+            Console.WriteLine("a is : {0}", a);
+            Console.WriteLine("b is : {0}", b);
+
+            Console.ReadLine();
+        }
+    }
+}
